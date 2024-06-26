a3d10a7571d9766818fc9624aac4458bf27b4c10
ngothinhphat <ngothinhphat244@gmail.com>
2024-06-26 19:09:49 +0700

    chore: Add dependencies for authentication, product routes, and database connection

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("hello world!")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const app = express();
+const env = process.env;
+const API = env.API_URL;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.options('*', cors());
+
+const authRouter = require('./routes/auth');
+const productRouter = require('./routes/products.js');
+
+app.use(`${API}/`, authRouter);
+
+app.use(`${API}/products`, productRouter);
+
+
+// Start the server
+// localhost >> 192.168.0.0
+const hostname = env.HOST;
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
